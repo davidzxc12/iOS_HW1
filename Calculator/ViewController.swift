507d829ac09319effd2d605b86d737f0540b5556
BryantWu <bryant@BryantWus-Mac.local>
2016-03-18 13:22:12 +0800

    homework1 v1.0

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var core: Core<Float>!
+    
+    func resetCore(){
+        self.core=Core()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resetCore()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var displaylabel: DisplayLabel!
+
+    @IBAction func NumberPressed(sender: UIButton) {
+        guard displaylabel.text!.characters.count < 11 else {
+            return
+        }
+        if sender.tag >= 1000 && sender.tag < 1010 {
+            displaylabel.append(sender.tag-1000)
+        }
+        else {
+            displaylabel.append(0)
+            displaylabel.append(0)
+        }
+        
     }
-
-
+    @IBAction func ACButtonClicked(sender: UIButton) {
+        displaylabel.clear()
+        resetCore()
+    }
+    
+    @IBAction func ChangeSignClicked(sender: UIButton) {
+        displaylabel.changeSign()
+    }
+    @IBAction func OperatorClicked(sender: UIButton) {
+        try! self.core.addStep(self.displaylabel.floatValue)
+        switch (sender.titleForState(.Normal)!) {
+        case "+":
+            try! self.core.addStep(+)
+        case "-":
+            try! self.core.addStep(-)
+        case "*":
+            try! self.core.addStep(*)
+        case "/":
+            try! self.core.addStep(/)
+        default:
+            break
+        }
+        displaylabel.clear()
+    }
+    @IBAction func CalculateClicked(sender: AnyObject) {
+        try! self.core.addStep(self.displaylabel.floatValue)
+        self.displaylabel.floatValue = try! self.core.calculate()
+        self.resetCore()
+    }
 }
 
